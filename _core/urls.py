db7836eff72a07326aeb4a58899a159b8d262a94
smattymatty <smattymatt@gmail.com>
2025-12-29 12:27:09 -0500

    fix redirect

--- _core/urls.py
+++ _core/urls.py
@@ -3,7 +3,6 @@ from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 from .sitemaps import SpellbookContentSitemap, StaticViewSitemap
 
@@ -16,9 +15,6 @@ urlpatterns = [
     path('a-panel/', admin.site.urls),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
-    # Legacy /philo/ redirects to /writing/philosophy/
-    path('philo/the-recursive-absurd/', RedirectView.as_view(
-        url='/writing/philosophy/the-recursive-absurd/', permanent=True)),
     path('', include('A_base.urls')),
     path('questions/', include('questions.urls')),
     path('', include('django_spellbook.urls')),
